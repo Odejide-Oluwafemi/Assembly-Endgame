fd8fa10d9e67787d6daa6795cbfead6b048112cb
LordDregg <oluwatosinodejide121@gmail.com>
2026-01-14 23:59:10 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./app.css";
+import Header from "./components/Header";
 
 export default function App() {
   return(
-    <h1>Hello! World</h1>
+    <main>
+      <Header/>
+    </main>
   );
 }
