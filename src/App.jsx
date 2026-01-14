46544fdc7055e9352f7e8ea930b5f12a7f4b0b4d
LordDregg <oluwatosinodejide121@gmail.com>
2026-01-15 00:02:25 +0100

    status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.css";
 import Header from "./components/Header";
+import Status from "./components/Status";
 
 export default function App() {
   return(
     <main>
       <Header/>
+      <Status/>
     </main>
   );
 }
